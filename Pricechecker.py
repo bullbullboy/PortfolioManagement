9513c7efe8aaef3ea4b27c335a8aea6dbca8767b
bullbullboy <gtrewqasdf@gmail.com>
2017-05-20 15:25:11 +0900

    add dictionary for same ticker

--- Pricechecker.py
+++ Pricechecker.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#for debug
+#import pdb;
+#pdb.set_trace()
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
@@ -9,9 +13,16 @@ YahooFinanceURL = 'https://finance.yahoo.com/quote/{0}/?p={0}'
 StockPriceClassName = 'Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)'
 
 class PriceChecker:
+    def __init__(self):
+        self.priceDictionary=dict()
+        
     def fetchPrice(self, ticker):
-        url = YahooFinanceURL.format(ticker)
-        html = urlopen(url)
-        soup = BeautifulSoup(html, "html.parser")
-        stockPrice = soup.find(class_ = StockPriceClassName);
-        return stockPrice.get_text()
+        if ticker in self.priceDictionary:
+            return self.priceDictionary[ticker]
+        else:
+            url = YahooFinanceURL.format(ticker)
+            html = urlopen(url)
+            soup = BeautifulSoup(html, "html.parser")
+            stockPrice = soup.find(class_ = StockPriceClassName);
+            self.priceDictionary[ticker] = stockPrice.get_text()
+            return stockPrice.get_text()
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import StockPricechecker
+import PriceChecker
 
 # main-routine
 
@@ -12,6 +12,6 @@ benchmarkTickerList = ['HDV', 'VTI', 'VT', 'VYM']
 tickerList = ownTickerList + benchmarkTickerList
 
 #printout ticker and stock price
+checker = PriceChecker.PriceChecker()
 for ticker in tickerList:
-    scraper = StockPricechecker.StockPriceChecker()
-    print(ticker + " : " + scraper.fetchPrice(ticker))
+    print(ticker + " : " + checker.fetchPrice(ticker))
